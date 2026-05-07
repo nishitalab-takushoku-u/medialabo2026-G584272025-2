4cd4e58c36adb55c95523ece7d905efa72fc5715
Okada Keito <258427@st.takushoku-u.ac.jp>
2026-05-07 16:29:01 +0900

    編集済み

--- practice-js/debug-practice.js
+++ practice-js/debug-practice.js
@@ -1,12 +1,12 @@
 let heikin = 3.5;
-console.log(Heikin);        // 正しくは heikin
+console.log(heikin);        // 正しくは heikin
 
 // (2) 関数名のつづり間違い      → エラー発生
-let h = Math.froor(heikin); // 正しくは floor
+let h = Math.floor(heikin); // 正しくは floor
 
 // (3) 存在しない配列要素       → undefined　（エラーにならない）
 let ary = ['a', 'b', 'c'];
-console.log(ary[3]);        // 3番目の要素は存在しない
+console.log(ary[2]);        // 3番目の要素は存在しない
 
 // (4) 小数のインデックス       → undefined　（エラーにならない）
 let i = 3/2;
@@ -14,10 +14,10 @@ console.log(ary[i]);        // i=3/2=1.5 なので ary[1.5] を参照しよう�
 
 // (5) メンバー名の間違い       → undefined （エラーにならない）
 let obj = {x:3, y:7};
-console.log(obj.z);         // メンバー z は存在しない
+console.log(obj);         // メンバー z は存在しない
 
 // (6) 初期化していない変数のメンバー   → エラー発生
-let o;
+let o=0;
 console.log(o.x);           // oは初期化していない
 
 // (7) オブジェクトや配列ではない値のメンバーと要素	→ undefined （エラーにならない）
@@ -30,5 +30,5 @@ let obj2 = {
 	mem1: {x:4, y:9},
 	mem2: "abc"
 };
-console.log(obj2.mem0.x);   // obj2.mem0 は存在しない. さらにそのメンバー x を参照しようとする
+console.log(obj2.mem1);   // obj2.mem0 は存在しない. さらにそのメンバー x を参照しようとする
 
